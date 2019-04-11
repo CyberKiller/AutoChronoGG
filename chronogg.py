61fe9c77cb6a97d842d4a436ee44092d06766317
CyberKiller <CyberKiller@users.noreply.github.com>
2019-04-11 21:42:24 +0100

    Added gmail oauth API
    Also changed some logging levels.
    Some minor refactoring.

--- chronogg.py
+++ chronogg.py
@@ -17,6 +17,88 @@ import urllib.request
 from email.message import EmailMessage
 from io import BytesIO
 
+#gmail api stuff
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+import base64
+from email.mime.text import MIMEText
+from apiclient import errors, discovery
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/gmail.send']
+CREDENTIALS_FILE_NAME = 'credentials.json'
+
+def init_gmail(config):
+    """Gets a gmail service object.
+    """
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                CREDENTIALS_FILE_NAME, SCOPES)
+            if config['email']['gmail']['console_oauth']:
+                creds = flow.run_console()
+            else:
+                creds = flow.run_local_server()
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('gmail', 'v1', credentials=creds)
+    return service
+
+def create_message(sender, to, subject, message_text):
+    """Create a message for an email.
+
+    Args:
+      sender: Email address of the sender.
+      to: Email address of the receiver.
+      subject: The subject of the email message.
+      message_text: The text of the email message.
+
+    Returns:
+      An object containing a base64url encoded email object.
+    """
+    message = MIMEText(message_text)
+    message['to'] = to
+    message['from'] = sender
+    message['subject'] = subject
+    raw = base64.urlsafe_b64encode(message.as_bytes())
+    return {'raw': raw.decode()}
+
+def send_message(service, user_id, message):
+    """Send an email message.
+
+    Args:
+      service: Authorized Gmail API service instance.
+      user_id: User's email address. The special value "me"
+      can be used to indicate the authenticated user.
+      message: Message to be sent.
+
+    Returns:
+      Sent Message.
+    """
+    try:
+        message = (service.users().messages().send(userId=user_id, body=message).execute())
+        logging.info('Message Id: ' + message['id'])
+        return message
+    except errors.HttpError as error:
+        logging.warning('An error occurred: ' + error)
+
+#end of gmail api stuff
+
 MAIN_URL = 'https://chrono.gg'
 POST_URL = 'https://api.chrono.gg/quest/spin'
 ALREADY_CLICKED_CODE = 420
@@ -78,7 +160,7 @@ def get_web_page(url, headers, cookies):
             r = response.read()
         return r
     except urllib.error.HTTPError as e:
-        logging.info(f"Error processing webpage: {e}")
+        logging.warning(f"Error processing webpage: {e}")
         if e.code == ALREADY_CLICKED_CODE:
             return ALREADY_CLICKED_CODE
         if e.code == UNAUTHORIZED:
@@ -119,7 +201,7 @@ def config_exists():
     return os.path.exists(CONFIG_FILE_NAME)
 
 
-def send_mail(config, subject, message):
+def send_mail(config, subject, body):
     if config and config['email']['enabled']:
         recipients = []
         for email in config['email']['to']:
@@ -128,17 +210,29 @@ def send_mail(config, subject, message):
             'name': config['email']['from']['name'],
             'address': config['email']['from']['address']
         }
+        frm = frm['name'] + ' <' + frm['address'] + '>'
+        to = ', '.join(recipients)
         try:
-            msg = EmailMessage()
-            msg['Subject'] = subject
-            msg['From'] = frm['name'] + ' <' + frm['address'] + '>'
-            msg['To'] = ', '.join(recipients)
-            msg.set_content(message)
-            server = smtplib.SMTP(config['email']['server'])
-            server.send_message(msg)
-            server.quit()
+            if config['email']['gmail']['enabled']:
+                msg = create_message(frm, to, subject, body)
+                send_message(init_gmail(config), 'me', msg)
+            else:
+                msg = EmailMessage()
+                msg['Subject'] = subject
+                msg['From'] = frm
+                msg['To'] = to
+                msg.set_content(body)
+                server = smtplib.SMTP(config['email']['server'])
+                server.send_message(msg)
+                server.quit()
+        except FileNotFoundError as e:
+            if (e.filename == CREDENTIALS_FILE_NAME):
+                logging.warning('An error occurred while sending an e-mail alert. '
+                             'Please go to: "https://developers.google.com/gmail/api/quickstart/python", '
+                             f'complete step 1 and save "{CREDENTIALS_FILE_NAME}" to the same directory '
+                             'as this script.')
         except:
-            logging.info('An error occurred while sending an e-mail alert.'
+            logging.warning('An error occurred while sending an e-mail alert.'
                          ' Please check your configuration file or your mail server.')
             raise
 
@@ -150,8 +244,8 @@ def main():
             if not config:
                 CONFIG_ERR_STR = (f'An error occurred while trying to load the config from file.'
                                   f' Check the JSON syntax in {CONFIG_FILE_NAME}.')
-                logging.info(CONFIG_ERR_STR)
-                send_mail(config=config, subject='AutoChronoGG: Config error', message=CONFIG_ERR_STR)
+                logging.error(CONFIG_ERR_STR)
+                send_mail(config=config, subject='AutoChronoGG: Config error', body=CONFIG_ERR_STR)
                 return
         if len(sys.argv) < 2:
             gg_cookie = get_cookie_from_file()
@@ -160,28 +254,28 @@ def main():
                                          'Usage: ./chronogg.py <Authorization Token>\n'
                                          'Please read the README.md and follow the instructions on '
                                          'how to extract your authorization token.')
-                logging.info(MISSING_TOKEN_ERR_STR)
-                send_mail(config=config, subject='AutoChronoGG: Missing token', message=MISSING_TOKEN_ERR_STR)
+                logging.warning(MISSING_TOKEN_ERR_STR)
+                send_mail(config=config, subject='AutoChronoGG: Missing token', body=MISSING_TOKEN_ERR_STR)
                 return
         else:
             gg_cookie = sys.argv[1]
 
         results = get_web_page(POST_URL, GLOBAL_HEADERS, gg_cookie)
         if not results:
-            logging.info('An unknown error occurred while fetching results. Terminating...')
+            logging.error('An unknown error occurred while fetching results. Terminating...')
             return
         elif results == ALREADY_CLICKED_CODE:
             ALREADY_CLICKED_ERR_STR = ('An error occurred while fetching results: Coin already clicked.'
                                        'Terminating...')
-            logging.info(ALREADY_CLICKED_ERR_STR)
+            logging.warning(ALREADY_CLICKED_ERR_STR)
             save_cookie(gg_cookie)
-            send_mail(config=config, subject='AutoChronoGG: Coin already clicked', message=ALREADY_CLICKED_ERR_STR)
+            send_mail(config=config, subject='AutoChronoGG: Coin already clicked', body=ALREADY_CLICKED_ERR_STR)
             return
         elif results == UNAUTHORIZED:
             UNAUTHORIZED_ERR_STR = ('An error occurred while fetching results: Expired/invalid authorization token.'
                                     ' Terminating...')
-            logging.info(UNAUTHORIZED_ERR_STR)
-            send_mail(config=config, subject='AutoChronoGG: Invalid token', message=UNAUTHORIZED_ERR_STR)
+            logging.warning(UNAUTHORIZED_ERR_STR)
+            send_mail(config=config, subject='AutoChronoGG: Invalid token', body=UNAUTHORIZED_ERR_STR)
             return
         logging.info('Done.')
         save_cookie(gg_cookie)
